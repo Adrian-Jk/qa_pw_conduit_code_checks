2103e57b93473bfe48927d61cb3ba789c83ac7d1
Adrian-Jk <adrianjuraszek88@gmail.com>
2026-04-16 10:41:56 +0200

    Update signInNegative.spec.js

--- tests/signIn/signInNegative.spec.js
+++ tests/signIn/signInNegative.spec.js
@@ -15,7 +15,7 @@ test.describe("Sign in negative tests", () => {
     await signInPage.assertErrorMessageContainsText(`password:can't be blank`);
   });
 
-  test("Assert error message for empty email", async ({ page }) => {
+  test("Assert error message for empty email", async () => {
     await signInPage.fillPasswordField("newpass123!");
     await signInPage.clickSignInButton();
     await signInPage.assertErrorMessageContainsText(`email:can't be blank`);
